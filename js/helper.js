5a4c11486b875c0e38af8bfa797d18ac0c12a1d2
Adam Betcher <adambetcher@theregularguys.biz>
2015-01-04 16:23:41 -0800

    1.11 added biography information

--- js/helper.js
+++ js/helper.js
@@ -27,8 +27,8 @@ var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
-
+//var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskills = '<li class="flex-item"><span class="black-text">%data%</span></li>';
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,36 @@
 var myName = HTMLheaderName.replace("%data%", "Adam J Betcher");
 var myRole = HTMLheaderRole.replace("%data%", "Software Architect");
-$("#main").prepend(myRole);
-$("#main").prepend(myName);
+var myEmail = HTMLemail.replace("%data%", "adambetcher@theregularguys.biz");
+var myMobile = HTMLmobile.replace("%data%", "555-666-7777");
+var myPictureURL = HTMLbioPic.replace("%data%", "http://www.theregularguys.biz/images/Adam.jpg");
+var myWelcomeMessage = HTMLWelcomeMsg.replace("%data%", "Welcome to my resume!");
+var mySkills = [
+		HTMLskills.replace("%data%", "Software Development")
+		, HTMLskills.replace("%data%", "Busienss Developship")
+		, HTMLskills.replace("%data%", "Software Architecture")
+		, HTMLskills.replace("%data%", "C#")
+		, HTMLskills.replace("%data%", "MVC.Net")
+	];
+
+var bio = {
+	name : myName
+	,role : myRole
+	,contactInfo : {
+		email : myEmail
+		, mobile : myMobile
+	}
+	,pictureURL : myPictureURL
+	,welcomeMessage : myWelcomeMessage
+	,skills : mySkills
+};
+
+
+$("#main").prepend(bio.role);
+$("#main").prepend(bio.name);
+$("#main").append(bio.contactInfo.myEmail);
+$("#main").append(bio.contactInfo.myMobile);
+$("#main").append(bio.pictureURL);
+$("#main").append(bio.welcomeMessage);
+$("#main").append(HTMLskillsStart);
+$("#skillsH3").append(bio.skills);
+
